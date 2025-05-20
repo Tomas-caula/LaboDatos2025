1afdb98dcd8e8f990cc9a5a196aa63f3f1aed3ed
Tomas-caula <70955134+Tomas-caula@users.noreply.github.com>
2025-05-20 13:08:48 -0300

    hot fix

--- Tp/main.py
+++ Tp/main.py
@@ -5,38 +5,58 @@ from phonenumbers import geocoder, carrier
 from inline_sql import sql, sql_val
 import duckdb
 
-basePathTomas = "C:/Users/tomas/OneDrive/Documents/GitHub/LaboDatos2025/Tp/"
-basePathIoel = "D:/Users/Usuario/Documents/GitHub/LaboDatos2025/Tp"
-basePathFran = "C:/Users/franc/Documents/GitHub/LaboDatos2025/Tp"
 
-basePath = basePathTomas
+
+basePath = "./"
 # basePath = basePathTomas  
 # basePath = basePathIoel
-
-
+invalidos = []
+establecimientos = pd.read_csv(basePath + "/estableciminetos.csv", encoding="latin1", low_memory=False)
 bibliotecas = pd.read_csv(basePath + "/bibliotecas.csv")
 
 def tabla_relaciones_EE():
-    establecimientos = pd.read_csv(basePath + "/estableciminetos.csv", encoding="latin1", low_memory=False)
     establecimientosEducativosComunes = "EEcomunes.csv"
 
     filtrados = []
-    filtrados.append(establecimientos.iloc[4, [4, 8, 9, 16, 17, 18, 19]])
+    filtrados.append(establecimientos.iloc[4, [4, 8, 9, 17, 18, 19]])
     for i in range(5, len(establecimientos)):
         if establecimientos.iloc[i, 13] == "1":
-            filtrados.append(establecimientos.iloc[i, [4, 8, 9, 16, 17, 18, 19]])
-        
+            filtrados.append(establecimientos.iloc[i, [4, 8, 9, 17, 18, 19]])
     filtrados_df = pd.DataFrame(filtrados)
-    filtrados_df.to_csv(establecimientosEducativosComunes, index=False, encoding="utf-8")
+    columnas = {filtrados_df.columns[0]: "codigo_departamento", filtrados_df.columns[1]:"nombre", filtrados_df.columns[2]:"domicilio", filtrados_df.columns[3]:"jardin_infantes", filtrados_df.columns[4]: "primario", filtrados_df.columns[5]:"secundario"}
+    filtrados_df = filtrados_df.rename(columns=columnas)
+    filtrados_df = filtrados_df[filtrados_df["secundario"] != "Secundario"]
+    filtrados_df.to_csv(establecimientosEducativosComunes, encoding="utf-8")
+
 
 def tabla_relaciones_BP():
-    
     bibliotecasPopulares = "BP.csv"
     filtrados = []
     for i in range(1, len(bibliotecas)):
       filtrados.append(bibliotecas.iloc[i, [2, 9, 10, 15, 22]])
     filtrados_df = pd.DataFrame(filtrados)
-    filtrados_df.to_csv(bibliotecasPopulares, index=False, encoding="utf-8")
+    filtrados_df.to_csv(bibliotecasPopulares, encoding="utf-8")
+    return filtrados_df
+
+
+def obtener_provincia(codigo):
+    localEstablecimiento =  establecimientos.iloc[5 :, 0: 5].dropna(thresh=1)
+    columnas = {localEstablecimiento.columns[0]:"jurisdiccion", localEstablecimiento.columns[1]:"sector", localEstablecimiento.columns[2]:"ambito", localEstablecimiento.columns[3]:"departamento", localEstablecimiento.columns[4]:"codigo_area"}
+    localEstablecimiento =  localEstablecimiento.rename(columns=columnas)
+    coincidencias = localEstablecimiento[localEstablecimiento["codigo_area"] == str(codigo)]
+    codigosCaba = ['02007', '02014', '02021', '02028', '02035', '02042', '02049', '02056', '02063', '02070', '02077', '02084', '02091', '02098']
+    if not coincidencias.empty:
+        return coincidencias["jurisdiccion"].iloc[0]
+    elif codigo == "94008":
+        invalidos.append(codigo)
+        return "Tierra del Fuego"
+    elif codigo in codigosCaba:
+        return "Ciudad de Buenos Aires"
+    else :
+        return "desconocida"
+
+    
+
 
 def tabla_departamentos():
     poblacion = pd.read_csv(basePath + "/poblacion.csv")
@@ -55,7 +75,6 @@ def tabla_departamentos():
     cuenta_jardin = 0 
     cuenta_primario = 0 
     cuenta_secundario = 0
-    print(poblacion)
     while index < len(poblacion):
         if "#" in poblacion["edad"][index]:
             df_departamentos.loc[len(df_departamentos)] = [area, nombre, cuenta_jardin, cuenta_primario, cuenta_secundario]
@@ -66,7 +85,6 @@ def tabla_departamentos():
             cuenta_secundario = 0
         elif 3 <= int(poblacion["edad"][index]) <= 5  :
             valor = str(poblacion["casos"][index]).replace(" ", "").strip()
-            print(valor)
             cuenta_jardin += int(valor)
         elif 5 <=int(poblacion["edad"][index]) <= 12: 
             valor = str(poblacion["casos"][index]).replace(" ", "").strip()
@@ -75,8 +93,8 @@ def tabla_departamentos():
             valor = str(poblacion["casos"][index]).replace(" ", "").strip()
             cuenta_secundario += int(valor)
         index += 1
-
-    print(df_departamentos)    
+    df_departamentos['provincia'] = df_departamentos['id_departamento'].apply(obtener_provincia)
+    df_departamentos.to_csv(departamentos, index=False, encoding="utf-8")
     
     # while poblacion.iloc[i].astype(str).str.startswith("AREA") == False:
     #     tabla_por_departamento = [area, nombre, poblacion_0_6, poblacion_6_12, ppoblacion_12_18]
@@ -120,13 +138,12 @@ def analizarMail ():
 #hacer una tabla provincia departamente cantidad de jardines, poblacion de edad de jardin, primaria 
 
 #print(consultaSQL.head(8))
-tabla_departamentos()
-
 #print(consultaSQL)
 
 #dataframeResultado = sql^ consultaSQL
 
 #print(dataframeResultado)
+tabla_relaciones_EE()
 
 
 
